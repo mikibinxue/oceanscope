0b10160affda8a26f7eb4aafe6a72449f2ae00cc
Miki Bin <xb285@nyu.edu>
2019-03-30 21:51:06 +0800

    add scene transport

---
+++ oceanscope/Assets/LevelUp.cs
@@ -0,0 +1,48 @@
+﻿// This script moves the GameObject you attach in the Inspector to a Scene you specify in the Inspector.
+// Attach this script to an empty GameObject.
+// Click on the GameObject, go to its Inspector and type the name of the Scene you would like to load in the Scene field.
+// Attach the GameObject you would like to move to a new Scene in the "My Game Object" field
+
+// Make sure your Scenes are in your build (File>Build Settings).
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Example : MonoBehaviour
+{
+    // Type in the name of the Scene you would like to load in the Inspector
+    public string level2;
+
+    // Assign your GameObject you want to move Scene in the Inspector
+    public GameObject Tiger;
+
+    void Update()
+    {
+        // Press the space key to add the Scene additively and move the GameObject to that Scene
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //StartCoroutine(LoadYourAsyncScene());
+        }
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+        // Set the current Scene to be able to unload it later
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // The Application loads the Scene in the background at the same time as the current Scene.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(level2, LoadSceneMode.Additive);
+
+        // Wait until the last operation fully loads to return anything
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
+        SceneManager.MoveGameObjectToScene(Tiger, SceneManager.GetSceneByName(level2));
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+    }
+}
---
+++ oceanscope/Assets/onTriggerLoad.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class onTriggerLoad : MonoBehaviour
+{
+    public GameObject enterText;
+    public string levelToLoad;
+
+    void Start()
+    {
+        enterText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Player")
+        {
+            enterText.SetActive(true);
+            if ((Input.GetKeyUp(KeyCode.Return)))
+            {
+                SceneManager.LoadScene(levelToLoad);
+            }
+        }
+    }
+    void OnTriggerExit(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Player")
+        {
+            enterText.SetActive(false);
+        }
+    }
+}
